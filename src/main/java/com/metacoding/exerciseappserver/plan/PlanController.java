3786fb21ca82bd408df2edebcb1ba82bfcb29764
angus <angus1208@naver.com>
2025-01-08 18:38:00 +0900

    [fix] 세션 유저 값을 받아 메인페이지 출력하도록 변경

--- src/main/java/com/metacoding/exerciseappserver/plan/PlanController.java
+++ src/main/java/com/metacoding/exerciseappserver/plan/PlanController.java
@@ -1,5 +1,8 @@
 package com.metacoding.exerciseappserver.plan;
+import com.metacoding.exerciseappserver._core.auth.SessionUser;
+import com.metacoding.exerciseappserver._core.error.exception.Exception403;
 import com.metacoding.exerciseappserver._core.util.ApiUtil;
+import com.metacoding.exerciseappserver.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,14 +13,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class PlanController {
     private final PlanService planService;
-    // TODO : 로그인 구현 완료 후 sessionUser 어노테이션 + 주석 해제
-    @GetMapping("/plan/user/{id}")
-    public ResponseEntity<?> userinfo(@PathVariable Integer id) {
-
+    @GetMapping("/plan/user")
+    public ResponseEntity<?> userinfo(@SessionUser User sessionUser) {
 //        if (sessionUser.getId() != id) {
 //            throw new Exception403("해당 정보에 접근할 권한이 없습니다 : "+id);
 //        }
-        ApiUtil.ApiResult<PlanResponse.UserWeekInfoDTO> resp = ApiUtil.success(planService.findUserWeekInfo(id));
+        ApiUtil.ApiResult<PlanResponse.UserWeekInfoDTO> resp = ApiUtil.success(planService.findUserWeekInfo(sessionUser.getId()));
         return ResponseEntity.ok(resp);
     }
 }
