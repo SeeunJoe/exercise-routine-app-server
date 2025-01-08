989f13b05ef57117655cf10ded69cd4eda22aaa5
angus <angus1208@naver.com>
2025-01-08 16:16:27 +0900

    [feat] 유저 운동 정보를 반환하도록 로직 추가(메인페이지)

---
+++ src/main/java/com/metacoding/exerciseappserver/_core/util/CommonData.java
@@ -0,0 +1,5 @@
+package com.metacoding.exerciseappserver._core.util;
+
+public class CommonData {
+    public static String[] weekNameArr = {"월", "화", "수", "목", "금", "토", "일"};
+}
--- src/main/java/com/metacoding/exerciseappserver/plan/PlanService.java
+++ src/main/java/com/metacoding/exerciseappserver/plan/PlanService.java
@@ -1,10 +1,33 @@
 package com.metacoding.exerciseappserver.plan;
 
+import com.metacoding.exerciseappserver._core.util.CommonData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class PlanService {
     private final PlanRepository planRepository;
+
+    public PlanResponse.UserWeekInfoDTO findUserWeekInfo(Integer userId) {
+
+        List<Plan> planList = planRepository.findUserWeekInfo(userId);
+
+        List<WeekInformation> weekInformationList = new ArrayList<>();
+        for (int i = 0; i < planList.size(); i++) {
+            List<String> fitnessNameList = new ArrayList<>();
+            for (Plan plan : planList) {
+                if (plan.getDayOfWeek().equals(CommonData.weekNameArr[i])) {
+                    fitnessNameList.add(plan.getFitness().getFitnessName());
+                }
+            }
+            weekInformationList.add(new WeekInformation(CommonData.weekNameArr[i], fitnessNameList));
+        }
+
+        return new PlanResponse.UserWeekInfoDTO(weekInformationList);
+    }
 }
