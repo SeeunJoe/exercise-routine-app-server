523a9bb228a3bed35018dcee2f767341bd7a6022
angus <angus1208@naver.com>
2025-01-09 17:57:47 +0900

    [feat] 테스트 폴더 및 클래스 작성

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,5 +1,5 @@
 insert into user_tb(username, password, email) values('ssar', '$2a$10$DDJOwWzVI3VE4jtDgc.OcOQymy1sbksVfA0uJ9tVF.p/WZiP1X3qy', 'ssar@nate.com');
-insert into user_tb(username, password, email) values('cos', '$2a$10$DDJOwWzVI3VE4jtDgc.OcOQymy1sbksVfA0uJ9tVF.p/WZiP1X3qy', 'cos@nate.com');
+insert into user_tb(username, password, email, height, weight) values('cos', '$2a$10$DDJOwWzVI3VE4jtDgc.OcOQymy1sbksVfA0uJ9tVF.p/WZiP1X3qy', 'cos@nate.com', 170, 70);
 insert into user_tb(username, password, email) values('love', '$2a$10$DDJOwWzVI3VE4jtDgc.OcOQymy1sbksVfA0uJ9tVF.p/WZiP1X3qy', 'love@nate.com');
 
 
