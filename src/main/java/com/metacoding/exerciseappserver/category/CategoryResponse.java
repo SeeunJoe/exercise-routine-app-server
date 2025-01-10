769d7b981b5d561883bcf47db25bd5c11579dab2
SeeunJoe <whtpdms1556@naver.com>
2025-01-10 15:47:00 +0900

    [fix]category와 fitness를 각각 보내는 것으로 수정

--- src/main/java/com/metacoding/exerciseappserver/category/CategoryResponse.java
+++ src/main/java/com/metacoding/exerciseappserver/category/CategoryResponse.java
@@ -1,4 +1,17 @@
 package com.metacoding.exerciseappserver.category;
 
+import lombok.Data;
+
 public class CategoryResponse {
+
+    @Data
+    public static class CategoryDTO {
+        private Integer categoryId;
+        private String categoryName;
+
+        public CategoryDTO(Category category) {
+            this.categoryId = category.getId();
+            this.categoryName = category.getName();
+        }
+    }
 }
--- src/main/java/com/metacoding/exerciseappserver/category/CategoryService.java
+++ src/main/java/com/metacoding/exerciseappserver/category/CategoryService.java
@@ -1,12 +1,24 @@
 package com.metacoding.exerciseappserver.category;
 
+import com.metacoding.exerciseappserver.fitness.FitnessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class CategoryService {
     private final CategoryRepository categoryRepository;
+
+    public List<CategoryResponse.CategoryDTO> getCategoryList() {
+
+        List<Category> categoryList = categoryRepository.getCategoryList();
+
+        return categoryList.stream()
+                .map(CategoryResponse.CategoryDTO::new)
+                .toList();
+    }
 }
